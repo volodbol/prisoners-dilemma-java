ce63b569e5988ae32b75b8cb8fbc2a97cbed141d
Volodymyr Bolshakov <volodbol@gmail.com>
2024-08-13 20:17:25 +0300

    feat: create basic structure

---
+++ src/test/java/com/volod/prisoners/PrisonersDilemmaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.volod.prisoners;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrisonersDilemmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
